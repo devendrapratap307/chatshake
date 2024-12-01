5864ff60fc54a06d4df882291f90fa432bfc026b
Pratap <pratapdevendra307@gmail.com>
2024-12-01 17:46:50 +0530

    first commit

---
+++ src/main/java/com/chatshake/chat/chatshake_app/ChatshakeAppApplication.java
@@ -0,0 +1,13 @@
+package com.chatshake.chat.chatshake_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatshakeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatshakeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatshake/chat/chatshake_app/ChatshakeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatshake.chat.chatshake_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatshakeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
