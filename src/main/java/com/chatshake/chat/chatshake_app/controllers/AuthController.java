b83e8b4b7a7d9a22cd5a818f48d79448f24cc37c
Pratap <pratapdevendra307@gmail.com>
2024-12-29 19:55:55 +0530

    unique user, auth

--- src/main/java/com/chatshake/chat/chatshake_app/controllers/AuthController.java
+++ src/main/java/com/chatshake/chat/chatshake_app/controllers/AuthController.java
@@ -4,9 +4,15 @@ import com.chatshake.chat.chatshake_app.dto.UserTO;
 import com.chatshake.chat.chatshake_app.models.User;
 import com.chatshake.chat.chatshake_app.repositories.UserRepository;
 import com.chatshake.chat.chatshake_app.services.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.validation.ValidationErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -28,16 +34,27 @@ public class AuthController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private Validator userValidator;
+
     @PostMapping("/login")
     public HashMap<String, String> login(@RequestParam String username, @RequestParam String password) {
         return authService.authenticate(username, password);
     }
 
     @PostMapping("/user/add")
-    public User addUser(@RequestBody UserTO user) {
+    public ResponseEntity<?> addUser(@RequestBody UserTO user, Errors result, HttpServletRequest request) {
+        if(userValidator.supports(UserTO.class)){
+            userValidator.validate(user,result);
+        }
+        if (result.hasErrors()) {
+//            ValidationErrors validationError = ;// need to create responseTO
+            return new ResponseEntity<>(result.getFieldError(), HttpStatus.OK);
+        }
         String encryptedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encryptedPassword);
-        return userRepository.save(this.modelMapper.map(user, User.class));
+        User userReturn = userRepository.save(this.modelMapper.map(user, User.class));
+        return new ResponseEntity<>(userReturn,HttpStatus.CREATED);
     }
 }
 
